42a0d9fe87d18949ad4427e7c648ca3bd2788018
eddy <111906334+wickedmotion@users.noreply.github.com>
2023-03-11 12:45:56 +0100

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/script.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/dist'
-  }
+    path: path.resolve(__dirname, 'dist')
+  },
   resolve: {
     fallback: {
       "path": require.resolve("path-browserify")
