38345c9a335d2578a2c63f197543ac8af40bf015
eddy <111906334+wickedmotion@users.noreply.github.com>
2023-03-11 17:57:27 +0100

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -23,6 +23,30 @@ const quizData = [
   },
 ];
 
+const trafficSignsContainer = document.getElementById("traffic-signs-container");
+
+// Loop through the images folder and create img elements with src set to the file names
+const imagesFolder = "/static/";
+const fileExtension = ".png";
+fetch(imagesFolder)
+  .then(response => response.text())
+  .then(text => {
+    const parser = new DOMParser();
+    const html = parser.parseFromString(text, "text/html");
+    const links = html.querySelectorAll("a");
+    links.forEach(link => {
+      const href = link.getAttribute("href");
+      if (href.endsWith(fileExtension)) {
+        const img = document.createElement("img");
+        img.src = imagesFolder + href;
+        img.alt = "Verkeersbord";
+        img.classList.add("traffic-sign");
+        img.id = href.slice(0, -fileExtension.length); // Set the id to the file name without the extension
+        trafficSignsContainer.appendChild(img);
+      }
+    });
+  });
+
 const quizContainer = document.getElementById("quiz-container");
 const quiz = new Quiz(quizData, quizContainer);
 quiz.render();
